052a3ebf897b100639c9073f44320312b28e01d4
omeriftikhar <omer.iftikhar3@gmail.com>
2018-10-31 00:54:15 +0500

    Initial commit

---
+++ app/src/main/java/com/veggiee/veggiee/AuthenticationActivity.java
@@ -0,0 +1,67 @@
+package com.veggiee.veggiee;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN=100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AuthUiSetup();
+
+    }
+
+    public void AuthUiSetup()
+    {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==RC_SIGN_IN)
+        {
+            IdpResponse response=IdpResponse.fromResultIntent(data);
+
+            if(resultCode==RESULT_OK)
+            {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                Intent categoryIntent=new Intent(AuthenticationActivity.this,CategoryActivity.class);
+                startActivity(categoryIntent);
+                finish();
+            }
+        }
+    }
+}

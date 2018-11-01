9e435e8d3d696ba3471a10b17a8cb8a06c39e94f
mdawarpervaiz <m.dawarpervaiz@gmail.com>
2018-11-01 20:44:43 +0500

    Merge branch 'master' of /home/mdawarpervaiz/AndroidStudioProjects/Veggiee with conflicts.

--- app/src/main/java/com/veggiee/veggiee/CompleteProfileActivity.java
+++ app/src/main/java/com/veggiee/veggiee/CompleteProfileActivity.java
@@ -1,20 +1,49 @@
 package com.veggiee.veggiee;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.veggiee.veggiee.Common.Common;
+import com.veggiee.veggiee.Model.User;
+
+import java.util.Objects;
 
 public class CompleteProfileActivity extends AppCompatActivity {
 
     AppCompatEditText emailEditText;
     AppCompatEditText phoneEditText;
     AppCompatEditText nameEditText;
+    AppCompatEditText addressEditText;
+    AppCompatButton proceedButton;
+    TextInputLayout emailTIP,nameTIP,numberTIP,addressTIP;
 
     FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase=FirebaseDatabase.getInstance();
+    DatabaseReference users=mDatabase.getReference("User");
+    String provider=null;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +53,17 @@ public class CompleteProfileActivity extends AppCompatActivity {
         emailEditText=findViewById(R.id.emailEditText);
         phoneEditText=findViewById(R.id.phoneEditText);
         nameEditText=findViewById(R.id.nameEditText);
+        addressEditText=findViewById(R.id.addressEditText);
+        proceedButton=findViewById(R.id.updateProfileButton);
+
+        emailTIP=findViewById(R.id.emailTextLayout);
+        nameTIP=findViewById(R.id.nameTextLayout);
+        numberTIP=findViewById(R.id.phoneTextLayout);
+        addressTIP=findViewById(R.id.addressTextLayout);
+
+
+        Common.currentUser=new User();
+
 
 
         //Firebase Auth
@@ -38,8 +78,6 @@ public class CompleteProfileActivity extends AppCompatActivity {
 
         fillPresentInfo();
 
-
-
     }
 
     private void fillPresentInfo()
@@ -51,23 +89,32 @@ public class CompleteProfileActivity extends AppCompatActivity {
             if(mUser.getEmail()!=null)
             {
                 emailEditText.setText(mUser.getEmail());
-                emailEditText.setClickable(false);
+                nameEditText.setText(mUser.getDisplayName());
+                emailTIP.setVisibility(View.GONE);
+                nameTIP.setVisibility(View.GONE);
             }
 
 
             if(mUser.getPhoneNumber()!=null)
             {
                 phoneEditText.setText(mUser.getPhoneNumber());
-                phoneEditText.setClickable(false);
+                numberTIP.setVisibility(View.GONE);
             }
 
             if(mUser.getDisplayName()!=null)
             {
                 nameEditText.setText(mUser.getDisplayName());
-                nameEditText.setClickable(false);
+                nameEditText.setFocusable(false);
             }
 
         }
+    }
+
+
+    private void saveUserToDB()
+    {
+
+
     }
 
 
